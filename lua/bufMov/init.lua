7b516ef6c4815f971c0db0e1e49a60c2f343d508
Theis <theisc@pm.me>
2022-10-23 05:45:22 +0200

    fix(bug): Sometimes moving would fail
    nvim_win_get_cursor giver the cursor position in the buffer, despite
    what the help page says, so changing to vim.fn.wincol and winline, now
    gives the position in the window.

--- lua/bufMov/init.lua
+++ lua/bufMov/init.lua
@@ -1,122 +1,120 @@
 local api = vim.api
 
 local getCursorPosition = function(direction)
-  local cursor = {}
+    local cursor = {}
 
-  if ( direction == 'up' ) then
-    cursor.x = api.nvim_win_get_position(0)[2] + api.nvim_win_get_cursor(0)[2]
-    cursor.y = api.nvim_win_get_position(0)[1]
-  elseif ( direction == 'down' ) then
-    cursor.x = api.nvim_win_get_position(0)[2] + api.nvim_win_get_cursor(0)[2]
-    cursor.y = api.nvim_win_get_position(0)[1] + api.nvim_win_get_height(0)
-  elseif ( direction == 'left' ) then
-    cursor.x = api.nvim_win_get_position(0)[2]
-    cursor.y = api.nvim_win_get_position(0)[1] + api.nvim_win_get_cursor(0)[1]
-  elseif ( direction == 'right' ) then
-    cursor.x = api.nvim_win_get_position(0)[2] + api.nvim_win_get_width(0)
-    cursor.y = api.nvim_win_get_position(0)[1] + api.nvim_win_get_cursor(0)[1]
-  end
+    if direction == "up" then
+        cursor.x = api.nvim_win_get_position(0)[2] + vim.fn.wincol()
+        cursor.y = api.nvim_win_get_position(0)[1]
+    elseif direction == "down" then
+        cursor.x = api.nvim_win_get_position(0)[2] + vim.fn.wincol()
+        cursor.y = api.nvim_win_get_position(0)[1] + api.nvim_win_get_height(0)
+    elseif direction == "left" then
+        cursor.x = api.nvim_win_get_position(0)[2]
+        cursor.y = api.nvim_win_get_position(0)[1] + vim.fn.winline()
+    elseif direction == "right" then
+        cursor.x = api.nvim_win_get_position(0)[2] + api.nvim_win_get_width(0)
+        cursor.y = api.nvim_win_get_position(0)[1] + vim.fn.winline()
+    end
 
-  cursor.tab = api.nvim_get_current_tabpage()
+    cursor.tab = api.nvim_get_current_tabpage()
 
-  return cursor
+    return cursor
 end
 
 local moveCursor = function(cursor, direction)
-  if ( direction == 'up' ) then
-    cursor.y = cursor.y - 1
-  elseif ( direction == 'down' ) then
-    cursor.y = cursor.y + 1
-  elseif ( direction == 'left' ) then
-    cursor.x = cursor.x - 1
-  elseif ( direction == 'right' ) then
-    cursor.x = cursor.x + 1
-  end
+    if direction == "up" then
+        cursor.y = cursor.y - 1
+    elseif direction == "down" then
+        cursor.y = cursor.y + 1
+    elseif direction == "left" then
+        cursor.x = cursor.x - 1
+    elseif direction == "right" then
+        cursor.x = cursor.x + 1
+    end
 
-  return cursor
+    return cursor
 end
 
 local cursorOnEdge = function(cursor, direction)
+    if direction == "up" and cursor.y == 0 then
+        return true
+    elseif direction == "down" and cursor.y == api.nvim_get_option("lines") - api.nvim_get_option("cmdheight") - 1 then
+        return true
+    elseif direction == "left" and cursor.x == 0 then
+        return true
+    elseif direction == "right" and cursor.x == api.nvim_get_option("columns") then
+        return true
+    end
 
-  if ( direction == 'up' and cursor.y == 0 ) then
-    return true
-  elseif ( direction == 'down' and cursor.y == api.nvim_get_option('lines') - api.nvim_get_option('cmdheight') - 1 ) then
-    return true
-  elseif ( direction == 'left' and cursor.x == 0 ) then
-    return true
-  elseif ( direction == 'right' and cursor.x == api.nvim_get_option('columns') ) then
-    return true
-  end
-
-  return false
+    return false
 end
 
 local collisionWithWindow = function(cursor, window)
+    if
+        cursor.x <= (window.x + window.width) -- checks if cursor is to the left of the window
+        and cursor.x >= window.x -- checks if cursor is to the right of the window
+        and cursor.y <= (window.y + window.height) -- checks if the cursor is height than the window
+        and cursor.y >= window.y -- checks if the cursor is lower than the window
+        and cursor.tab == window.tab
+    then -- checks if cursor and window is on the same tab
+        return true
+    end
 
-  if ( cursor.x <= ( window.x + window.width ) and   -- checks if cursor is to the left of the window
-       cursor.x >= window.x and                      -- checks if cursor is to the right of the window
-       cursor.y <= ( window.y + window.height ) and  -- checks if the cursor is height than the window
-       cursor.y >= window.y and                      -- checks if the cursor is lower than the window
-       cursor.tab == window.tab ) then              -- checks if cursor and window is on the same tab
-    return true
-  end
-
-  return false
+    return false
 end
 
 local getWindowList = function()
-  local windows = {}
-  local windowList = api.nvim_list_wins()
+    local windows = {}
+    local windowList = api.nvim_list_wins()
 
-  for i, win in pairs(windowList) do
-    local window = {}
-    window.id = win
-    window.x = api.nvim_win_get_position(win)[2]
-    window.y = api.nvim_win_get_position(win)[1]
-    window.width = api.nvim_win_get_width(win)
-    window.height = api.nvim_win_get_height(win)
-    window.tab = api.nvim_win_get_tabpage(win)
+    for i, win in pairs(windowList) do
+        local window = {}
+        window.id = win
+        window.x = api.nvim_win_get_position(win)[2]
+        window.y = api.nvim_win_get_position(win)[1]
+        window.width = api.nvim_win_get_width(win)
+        window.height = api.nvim_win_get_height(win)
+        window.tab = api.nvim_win_get_tabpage(win)
 
-    windows[i] = window
-  end
+        windows[i] = window
+    end
 
-  return windows
+    return windows
 end
 
 local collisionWithAnyWindow = function(cursor)
+    local windows = getWindowList()
 
-  local windows = getWindowList()
-
-  for _, window in pairs(windows) do
-    if ( collisionWithWindow(cursor, window) ) then
-       return window
-     end
-  end
+    for _, window in pairs(windows) do
+        if collisionWithWindow(cursor, window) then
+            return window
+        end
+    end
 end
 
 local M = {}
 
 M.movBuf = function(direction)
+    local currentBuffer = api.nvim_get_current_buf()
+    local currentWindow = api.nvim_get_current_win()
 
-  local currentBuffer = api.nvim_get_current_buf()
-  local currentWindow = api.nvim_get_current_win()
+    local cursor = getCursorPosition(direction)
 
-  local cursor = getCursorPosition(direction)
+    if cursorOnEdge(cursor, direction) then
+        return
+    end
 
-  if ( cursorOnEdge(cursor, direction) ) then
-    return
-  end
+    local targetWindow = collisionWithAnyWindow(cursor)
+    while targetWindow.id == currentWindow do
+        cursor = moveCursor(cursor, direction)
+        targetWindow = collisionWithAnyWindow(cursor)
+    end
 
-  local targetWindow = collisionWithAnyWindow(cursor)
-  while ( targetWindow.id == currentWindow ) do
-    cursor = moveCursor(cursor, direction)
-    targetWindow = collisionWithAnyWindow(cursor)
-  end
-
-  local targetBuffer = api.nvim_win_get_buf(targetWindow.id)
-  api.nvim_win_set_buf(targetWindow.id, currentBuffer)
-  api.nvim_win_set_buf(currentWindow, targetBuffer)
-  api.nvim_set_current_win(targetWindow.id)
+    local targetBuffer = api.nvim_win_get_buf(targetWindow.id)
+    api.nvim_win_set_buf(targetWindow.id, currentBuffer)
+    api.nvim_win_set_buf(currentWindow, targetBuffer)
+    api.nvim_set_current_win(targetWindow.id)
 end
 
 return M
--- plugin/bufMov.lua
+++ plugin/bufMov.lua
@@ -1,4 +1,4 @@
-vim.cmd [[ command! MoveBufferUp lua require('bufMov').movBuf('up') ]]
-vim.cmd [[ command! MoveBufferDown lua require('bufMov').movBuf('down') ]]
-vim.cmd [[ command! MoveBufferLeft lua require('bufMov').movBuf('left') ]]
-vim.cmd [[ command! MoveBufferRight lua require('bufMov').movBuf('right') ]]
+vim.cmd([[ command! MoveBufferUp lua require('bufMov').movBuf('up') ]])
+vim.cmd([[ command! MoveBufferDown lua require('bufMov').movBuf('down') ]])
+vim.cmd([[ command! MoveBufferLeft lua require('bufMov').movBuf('left') ]])
+vim.cmd([[ command! MoveBufferRight lua require('bufMov').movBuf('right') ]])
