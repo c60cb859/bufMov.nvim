e86071806253f987636e7a594c17d13f0e8610d7
Theis <theisc@pm.me>
2022-10-23 07:15:24 +0200

    Remove(vimscript): Remove the last bit of vimscript

--- plugin/bufMov.lua
+++ plugin/bufMov.lua
@@ -1,4 +1,30 @@
-vim.cmd([[ command! MoveBufferUp lua require('bufMov').movBuf('up') ]])
-vim.cmd([[ command! MoveBufferDown lua require('bufMov').movBuf('down') ]])
-vim.cmd([[ command! MoveBufferLeft lua require('bufMov').movBuf('left') ]])
-vim.cmd([[ command! MoveBufferRight lua require('bufMov').movBuf('right') ]])
+local loaded = _G["bufMov_loaded"]
+
+if loaded then
+    return
+end
+
+local bufMov = require("bufMov")
+
+local up = function()
+    bufMov.movBuf("up")
+end
+
+local down = function()
+    bufMov.movBuf("down")
+end
+
+local left = function()
+    bufMov.movBuf("left")
+end
+
+local right = function()
+    bufMov.movBuf("right")
+end
+
+vim.api.nvim_create_user_command("MoveBufferUp", up, {})
+vim.api.nvim_create_user_command("MoveBufferDown", down, {})
+vim.api.nvim_create_user_command("MoveBufferLeft", left, {})
+vim.api.nvim_create_user_command("MoveBufferRight", right, {})
+
+_G["bufMov_loaded"] = true
